b444a2c51719869a64638ed73b4ff894062356ef
jeilones <jeisolon.espi@gmail.com>
2017-01-22 17:29:11 +0100

    Initialize Map
    Added the structure for the coding and the initialize mapping created by
    Cristhian, with the map and cell size and the legends.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# landUse-activityBaseModel
+# Activity-Base Model to simulate the Land Use
+
+Initializing the map:
+
+Open the windows console and type: `mapattr new.map`
+
+![Alt text](images/createNewMap.png?raw=true "Default values boundaries and cell size")
+
+You can define the size of the map and the also the cell size among others attributes.
+
+
+Base on the table rule (see src/initModel/LandUses.tbl):
+
+[0,0.05> 1
+[0.05,0.1> 2 
+[0.1,0.2> 3
+[0.2,1.0] 4
+
+Where:
+
+1 referes to Residential land use
+2 to Industrial land use
+3 to Agricultural land use and
+4 to Natural 
+
+We create our initial map call LandUse.map (see src/initModel/modelInitialization.py). As this  initial map is nominal, we need to add the legen label by typing the comand: `legend new.map`
+
+![Alt text](images/addLegendsToNominalMap.png?raw=true "Adding legends")
---
+++ src/initModel/modelInitialization.py
@@ -0,0 +1,58 @@
+from pcraster import *
+from pcraster.framework import *
+
+class RandomModel(DynamicModel):
+  def __init__(self):
+    DynamicModel.__init__(self)
+    setclone('../../resources/clone.map')
+
+  def initial(self):
+    #Creating basic land use map using the probabilities of the file LandUse, under a uniform distribution
+    uni=uniform(1)
+    landUses=lookupnominal('../../resources/LandUses.tbl', uni)
+    self.report(landUses, '../../src/LandUses')
+    
+    #Defining Boleean Maps
+    
+    residential = landUses == 1
+    
+    industrial = landUses == 2
+    
+    agricultural = landUses == 3
+   
+    natural= landUses == 4
+   
+    
+    #Creating starting population and job maps for each type of land use, feel free to change them
+
+    #Obviamente al inicializar los valores en las zonas residenciales existira
+    #una poblacion mayor mientras que en la region natural no vive nadie,
+    #y hay mos trabajos en la zona residencial
+    residentialPopulation = ifthenelse(residential, scalar(200), 0)
+    residentialJobs = ifthenelse(residential, scalar(10), 0)
+
+    industrialPopulation = ifthenelse(industrial, scalar(50), 0)
+    industrialJobs = ifthenelse(industrial, scalar(10), 0)
+
+    agriculturalPopulation = ifthenelse(agricultural, scalar(10), 0)
+    agriculturalJobs = ifthenelse(agricultural, scalar(30), 0)
+    #Natural es 0 en poblacion y 0 en trabajo por eso se omite este el
+    #calculo del mapa boleano para naturaleza
+    
+    populationMap = residentialPopulation + industrialPopulation + agriculturalPopulation
+    jobsMap = residentialJobs + industrialJobs + agriculturalJobs
+    
+    self.report(populationMap, 'populationMap')
+    self.report(jobsMap, 'jobsMap')
+    
+    
+
+  def dynamic(self):
+    u=uniform(1)
+   
+
+nrOfTimeSteps=10
+myModel = RandomModel()
+dynamicModel = DynamicFramework(myModel,nrOfTimeSteps)
+dynamicModel.run()
+
